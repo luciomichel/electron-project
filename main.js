65eef53a4e8e697491d33354a4788adf08603ed3
Michel Lucio <lucio.michel@gmail.com>
2022-12-26 11:41:56 -0300

    Versão 1.0.1

--- main.js
+++ main.js
@@ -1,69 +1,74 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
+const {app, BrowserWindow, dialog} = require('electron')
+const path = require('path')
+const {autoUpdater} = require('electron-updater')
+const log = require('electron-log')
+const os = require("os");
+log.transports.file.resolvePath = () => path.join(os.homedir(), 'auto-update-electron.log')
+log.info('Versão da aplicação = ' + app.getVersion())
+let win;
 
-let mainWindow;
-
-function createWindow () {
-
-  console.log("Criando Tela ")
-
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  
-  mainWindow.loadFile('index.html');
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-
-  mainWindow.once('ready-to-show', () => {
-    console.log("Verificando versão...")
-    autoUpdater.checkForUpdatesAndNotify();
-  });
+function createWindow() {
+    win = new BrowserWindow({width: 300, height: 400})
+    win.loadFile(path.join(__dirname, 'index.html'))
 }
 
+autoUpdater.autoDownload = false;
+
 app.on('ready', () => {
-  createWindow();
-});
-
-app.on('window-all-closed', function () {
-  console.log("fechando")
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-  
-});
-
-app.on('activate', function () {
-  if (mainWindow === null) {
     createWindow();
-  }
-});
-
-ipcMain.on('app_version', (event) => {
-  console.log("Versão", app.getVersion());
-  event.sender.send('app_version', { version: app.getVersion() });
-});
-
-ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-});
+    autoUpdater.checkForUpdates().then(function (r) {
+        return log.info('finalizou check update');
+    })
+})
 
 autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-});
+    log.info('update-available')
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Nova versão disponível',
+        message: 'Deseja realizar o download da versão agora?',
+        buttons: ['Sim', 'Não']
+    }).then(result => {
+        let buttonIndex = result.response
+        if (buttonIndex === 0) autoUpdater.downloadUpdate()
+    });
+})
+
+autoUpdater.on('update-not-available', () => {
+    log.info('update-not-available')
+})
+
+autoUpdater.on('checking-for-update', () => {
+    log.info('checking-for-update')
+})
+
+autoUpdater.on('download-progress', (progressTrack) => {
+    log.info('\n\ndownload-progress')
+    log.info(progressTrack)
+})
 
 autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-});
+    log.info('update-downloaded')
+    dialog.showMessageBox({
+        type: 'info',
+        title: 'Nova versão baixada',
+        message: 'Deseja instalar agora??',
+        buttons: ['Sim', 'Não']
+    }).then(result => {
+        let buttonIndex = result.response
+        if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true)
+    });
+})
 
-autoUpdater.on('error', (message) => {
-  mainWindow.webContents.send(message);
-  console.error('There was a problem updating the application')
-  console.error(message)
+autoUpdater.on('appimage-filename-updated', () => {
+    log.info('appimage-filename-updated')
+})
+
+autoUpdater.on('update-cancelled', () => {
+    log.info('update-cancelled')
+})
+
+autoUpdater.on('error', (e) => {
+    log.info('Erro no auto-update ' + e)
+    console.log(e);
 })
